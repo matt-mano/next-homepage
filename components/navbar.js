76c99e3673f0f2f838c0be8c735f227a79d87d14
Matthew Manoleras <matt.manoleras@outlook.com>
2022-07-17 10:26:38 -0400

    Added burger menu to navbar, style changes

--- components/navbar.js
+++ components/navbar.js
@@ -1,50 +1,67 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import React from 'react';
 
 export default function Navbar({ children }) {
     const router = useRouter();
     const currentRoute = router.pathname;
 
+    const [navbarActive, setNavbarActive] = React.useState(false);
+
+    const toggleNavbar = () => setNavbarActive(!navbarActive);
+
     return (<div>
-        <Link href="/">
-            <h1 className='title is-1 has-text-centered' style={{ 'cursor': 'pointer' }}>Matt Manoleras</h1>
-        </Link>
+
         <nav className="navbar" role="navigation">
             <div className="navbar-brand">
                 <div className="navbar-item">
                     <Link href="/">
-                        <a className={currentRoute === "/"
-                            ? "active"
-                            : "non-active"}>Home</a>
+                        <h2 className='title is-2' style={{ 'cursor': 'pointer' }}>Matt Manoleras</h2>
                     </Link>
                 </div>
-                <div className="navbar-item">
-                    <Link href="/experience">
-                        <a className={currentRoute === "/experience"
-                            ? "active"
-                            : "non-active"}>Experience</a>
-                    </Link>
-                </div>
-                <div className="navbar-item">
-                    <Link href="/education">
-                        <a className={currentRoute === "/education"
-                            ? "active"
-                            : "non-active"}>Education</a>
-                    </Link>
-                </div>
-                <div className="navbar-item">
-                    <Link href="/skills">
-                        <a className={currentRoute === "/skills"
-                            ? "active"
-                            : "non-active"}>Skills</a>
-                    </Link>
-                </div>
-                <div className="navbar-item">
-                    <Link href="/contact">
-                        <a className={currentRoute === "/contact"
-                            ? "active"
-                            : "non-active"}>Contact</a>
-                    </Link>
+                <a role="button" onClick={toggleNavbar} className={`navbar-burger ${navbarActive ? 'is-active' : ''}`}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </a>
+            </div>
+            <div className={`navbar-menu ${navbarActive ? 'is-active' : ''}`}>
+                <div className='navbar-end'>
+                    <div className="navbar-item" onClick={toggleNavbar}>
+                        <Link href="/">
+                            <a className={currentRoute === "/"
+                                ? "active"
+                                : "non-active"}>Home</a>
+                        </Link>
+                    </div>
+                    <div className="navbar-item" onClick={toggleNavbar}>
+                        <Link href="/experience">
+                            <a className={currentRoute === "/experience"
+                                ? "active"
+                                : "non-active"}>Experience</a>
+                        </Link>
+                    </div>
+                    <div className="navbar-item" onClick={toggleNavbar}>
+                        <Link href="/education">
+                            <a className={currentRoute === "/education"
+                                ? "active"
+                                : "non-active"}>Education</a>
+                        </Link>
+                    </div>
+                    <div className="navbar-item" onClick={toggleNavbar}>
+                        <Link href="/skills">
+                            <a className={currentRoute === "/skills"
+                                ? "active"
+                                : "non-active"}>Skills</a>
+                        </Link>
+                    </div>
+                    <div className="navbar-item" onClick={toggleNavbar}>
+                        <Link href="/contact">
+                            <a className={currentRoute === "/contact"
+                                ? "active"
+                                : "non-active"}>Contact</a>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </nav>
