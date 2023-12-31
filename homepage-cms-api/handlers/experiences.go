9e79ea78ae09e39140b4b080381346a8d038c393
Matthew Manoleras <matt.manoleras@outlook.com>
2023-12-30 20:16:17 -0500

    Beginning of db connection refactor

--- homepage-cms-api/handlers/experiences.go
+++ homepage-cms-api/handlers/experiences.go
@@ -6,52 +6,17 @@ import (
 	"homepagecms/models"
 	"log"
 	"net/http"
-	"regexp"
-	"strconv"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Experiences struct {
-	l *log.Logger
+	l  *log.Logger
+	db *mongo.Client
 }
 
-func NewExperiences(l *log.Logger) *Experiences {
-	return &Experiences{l}
-}
-
-func (exp *Experiences) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
-	if h.Method == http.MethodGet {
-		exp.getExperiences(rw, h)
-		return
-	}
-
-	if h.Method == http.MethodPost {
-		exp.addExperience(rw, h)
-		return
-	}
-
-	if h.Method == http.MethodPut {
-		//Let's get the id
-		reg := regexp.MustCompile(`/([0-9]+)`)
-		uri := h.URL.Path
-		g := reg.FindAllStringSubmatch(uri, -1)
-
-		if len(g) != 1 || len(g[0]) < 2 {
-			http.Error(rw, "Invalid URL", http.StatusBadRequest)
-			return
-		}
-
-		idString := g[0][1]
-		id, e := strconv.Atoi(idString)
-
-		if e != nil {
-			http.Error(rw, "Invalid URL", http.StatusBadRequest)
-			return
-		}
-
-		exp.l.Println("ID: ", id)
-	}
-
-	rw.WriteHeader(http.StatusNotImplemented)
+func NewExperiences(l *log.Logger, db *mongo.Client) *Experiences {
+	return &Experiences{l, db}
 }
 
 func (exp *Experiences) getExperiences(rw http.ResponseWriter, h *http.Request) {
@@ -76,4 +41,21 @@ func (exp *Experiences) addExperience(rw http.ResponseWriter, h *http.Request) {
 
 	exp.l.Printf("Prod: %#v", experience)
 	data.AddExperience(experience)
+}
+
+func (exp *Experiences) updateExperience(rw http.ResponseWriter, h *http.Request) {
+	//TODO: Stuff
+}
+
+func (exp *Experiences) getAnyExperience(rw http.ResponseWriter, h *http.Request) {
+	loaded := data.GetOneExperience(exp.db)
+
+	rw.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(rw)
+	err := encoder.Encode(loaded)
+
+	if err != nil {
+		http.Error(rw, "Couldn't read the JSONs from mongo", http.StatusBadRequest)
+	}
+
 }
